2d68fef31eb5015001d54118580f1dfc478cda8f
Michael Seutin <frenchmike@gmail.com>
2024-09-23 15:22:47 -0700

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 }
